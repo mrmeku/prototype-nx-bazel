3b25dae4a30ecddb511fcb2b8a47df859b714318
mrmeku <mrmeku@gmail.com>
2019-11-11 13:22:49 -0700

    Add reactapp to repo

--- apps/angularapp/src/app/app.component.ts
+++ apps/angularapp/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { SHARED_SYMBOL } from '@prototype-nx-bazel/shared-lib';
 @Component({
   selector: 'prototype-nx-bazel-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angularapp' + SHARED_SYMBOL;
